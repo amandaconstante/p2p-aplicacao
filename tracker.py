8f00fc73a5e3f48ba32463b2bd4e417209324aad
Amanda <89360779+amandaconstante@users.noreply.github.com>
2025-07-02 16:08:26 -0300

    feat: adc funcionalidade para remover peers inativos do tracker

--- tracker.py
+++ tracker.py
@@ -1,35 +1,57 @@
 import socket
 import json
-# import threading
+import time
+import threading
 
 # dicionário
 # formato: `{(ip, porta): ["a.txt", "b.txt"]}`
 peers = {}
+TIMEOUT = 10 # segundos sem atualização do peer, considera desconectado
 
-#def handle_tracker():
 trackerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 trackerSocket.bind(("0.0.0.0", 9010)) # porta fixa do tracker
 
-print("[TRACKER] Aguardando mensagem na porta 9010...");
+print("[TRACKER] Aguardando mensagem na porta 9010...")
+
+def remove_inativos():
+    while True:
+        agora = time.time()
+        inativos = [peer for peer, dados in peers.items() if agora - dados["ultimo_update"] > TIMEOUT]
+
+        for peer in inativos:
+            print(f"[TRACKER] Removendo peer inativo: {peer}")
+            del peers[peer]
+        
+        time.sleep(5)
+
+# inicia thread de análise de inatividade de peers
+threading.Thread(target=remove_inativos, daemon=True).start()
 
 while True: 
     data, addr = trackerSocket.recvfrom(4096)
     msg = json.loads(data.decode())
 
+    peer_ip = addr[0]
+    peer_porta = msg["peer"][1]
+    peer_id = (peer_ip, peer_porta)
+
     if msg["type"] == "register":
-        peer_id = tuple(msg["peer"])
+      #  peer_id = tuple(msg["peer"])
         arquivos = msg["arquivos"]
-        peers[peer_id] = arquivos
+        peers[peer_id] = {
+            "arquivos": arquivos,
+            "ultimo_update": time.time()
+        }
         print(f"[TRACKER] Peer registrado: {peer_id} com arquivos {arquivos}")
 
     elif msg["type"] == "get_peers":
-        solicitante = tuple(msg["peer"])
+       # solicitante = tuple(msg["peer"])
+        solicitante = peer_id
         lista = [
-            {"peer": list(k), "arquivos": v}
+            {"peer": list(k), "arquivos": v["arquivos"]}
             for k, v in peers.items()
             if k != solicitante
         ]
+        
         trackerSocket.sendto(json.dumps(lista[:3]).encode(), addr)
-
-# roda tracker em uam threar (poderia ser o main direto tb)
-# threading.Thread(target=handle_tracker).start()
+        print(f"[TRACKER] Enviou lista de peers para {solicitante}")
